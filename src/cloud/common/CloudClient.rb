bfdcfbd7ed006cf5d6c686a79d78c5328b55f589
Kristian Feldsam <feldsam@gmail.com>
2020-05-23 19:04:33 +0200

    Bump version 5.10.4.5
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -51,7 +51,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.10.4.4'
+    VERSION = '5.10.4.5'
 
     # #########################################################################
     # Default location for the authentication file
