740cd9122fb668768ec706ef474d678390dc3019
Sergio Betanzos <sbetanzos@opennebula.io>
2022-02-01 19:29:32 +0100

    F #5593: Implement OneProvision GUI to add hosts & IPs (#1739)
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>
    
    # Conflicts:
    #	src/fireedge/src/client/components/Forms/Provision/DeleteForm/schema.js
    #	src/fireedge/src/client/containers/Provisions/DialogInfo/hosts.js
    #	src/fireedge/src/client/containers/Provisions/DialogInfo/networks.js
    #	src/fireedge/src/client/features/One/provision/actions.js
    #	src/fireedge/src/client/features/One/provision/hooks.js

--- src/fireedge/src/client/containers/Provisions/DialogInfo/networks.js
+++ src/fireedge/src/client/containers/Provisions/DialogInfo/networks.js
@@ -13,29 +13,41 @@
  * See the License for the specific language governing permissions and       *
  * limitations under the License.                                            *
  * ------------------------------------------------------------------------- */
-import { memo, useEffect } from 'react'
+import { memo, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
-import { Trash as DeleteIcon } from 'iconoir-react'
+import { Trash as DeleteIcon, AddCircledOutline } from 'iconoir-react'
+import { Stack, TextField } from '@mui/material'
 
-import { useFetchAll } from 'client/hooks'
+import { useFetch, useFetchAll } from 'client/hooks'
 import { useVNetworkApi, useProvisionApi } from 'client/features/One'
 import { useGeneralApi } from 'client/features/General'
+import { SubmitButton } from 'client/components/FormControl'
 import { ListCards } from 'client/components/List'
 import { NetworkCard } from 'client/components/Cards'
+import { Translate } from 'client/components/HOC'
+import { T } from 'client/constants'
 
 const Networks = memo(
   ({ hidden, data, reloading, refetchProvision, disableAllActions }) => {
-    const {
-      networks = []
-    } = data?.TEMPLATE?.BODY?.provision?.infrastructure
+    const [amount, setAmount] = useState(() => 1)
+    const { networks = [] } = data?.TEMPLATE?.BODY?.provision?.infrastructure
 
     const { enqueueSuccess } = useGeneralApi()
-    const { deleteVNetwork } = useProvisionApi()
+    const { deleteVNetwork, addIp } = useProvisionApi()
     const { getVNetwork } = useVNetworkApi()
 
+    const { fetchRequest, loading: loadingAddIp } = useFetch(
+      async (payload) => {
+        await addIp(data?.ID, payload)
+        await refetchProvision()
+        enqueueSuccess(`IP added ${amount}x`)
+      }
+    )
+
     const { data: list, fetchRequestAll, loading } = useFetchAll()
-    const fetchVNetworks = () => fetchRequestAll(networks?.map(({ id }) => getVNetwork(id)))
+    const fetchVNetworks = () =>
+      fetchRequestAll(networks?.map(({ id }) => getVNetwork(id)))
 
     useEffect(() => {
       !hidden && !list && fetchVNetworks()
@@ -46,24 +58,56 @@ const Networks = memo(
     }, [reloading])
 
     return (
-      <ListCards
-        list={list}
-        isLoading={!list && loading}
-        CardComponent={NetworkCard}
-        cardsProps={({ value: { ID } }) => !disableAllActions && ({
-          actions: [{
-            handleClick: () => deleteVNetwork(ID)
-              .then(refetchProvision)
-              .then(() => enqueueSuccess(`VNetwork deleted - ID: ${ID}`)),
-            icon: <DeleteIcon color='error' />,
-            cy: `provision-vnet-delete-${ID}`
-          }]
-        })}
-        displayEmpty
-        breakpoints={{ xs: 12, md: 6 }}
-      />
+      <>
+        <Stack direction="row" mb="0.5em">
+          <TextField
+            type="number"
+            inputProps={{ 'data-cy': 'amount' }}
+            onChange={(event) => {
+              const newAmount = event.target.value
+              ;+newAmount > 0 && setAmount(newAmount)
+            }}
+            value={amount}
+          />
+          <Stack alignSelf="center">
+            <SubmitButton
+              data-cy="add-ip"
+              color="secondary"
+              endicon={<AddCircledOutline />}
+              label={<Translate word={T.AddIP} />}
+              sx={{ ml: 1, display: 'flex', alignItems: 'flex-start' }}
+              isSubmitting={loadingAddIp}
+              onClick={() => fetchRequest(amount)}
+            />
+          </Stack>
+        </Stack>
+        <ListCards
+          list={list}
+          isLoading={!list && loading}
+          CardComponent={NetworkCard}
+          cardsProps={({ value: { ID } }) =>
+            !disableAllActions && {
+              actions: [
+                {
+                  handleClick: () =>
+                    deleteVNetwork(ID)
+                      .then(refetchProvision)
+                      .then(() =>
+                        enqueueSuccess(`VNetwork deleted - ID: ${ID}`)
+                      ),
+                  icon: <DeleteIcon color="error" />,
+                  cy: `provision-vnet-delete-${ID}`,
+                },
+              ],
+            }
+          }
+          displayEmpty
+          breakpoints={{ xs: 12, md: 6 }}
+        />
+      </>
     )
-  }, (prev, next) =>
+  },
+  (prev, next) =>
     prev.hidden === next.hidden && prev.reloading === next.reloading
 )
 
@@ -72,7 +116,7 @@ Networks.propTypes = {
   hidden: PropTypes.bool,
   refetchProvision: PropTypes.func,
   reloading: PropTypes.bool,
-  disableAllActions: PropTypes.bool
+  disableAllActions: PropTypes.bool,
 }
 
 Networks.defaultProps = {
@@ -80,7 +124,7 @@ Networks.defaultProps = {
   hidden: false,
   refetchProvision: () => undefined,
   reloading: false,
-  disableAllActions: false
+  disableAllActions: false,
 }
 
 Networks.displayName = 'Networks'
