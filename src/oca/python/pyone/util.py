89251e89522a8ab3abf023413d110c76e4a16046
Kristian Feldsam <feldsam@gmail.com>
2022-05-30 22:08:34 +0200

    Revert "M #-: PyONE: rename buildChildren _buildChildren (#1393)"
    This reverts commit 172fe4a2f2754fbcb05cd7648d048430263ec7ed.
    
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>
    
    # Conflicts:
    #	src/oca/python/pyone/util.py

--- src/oca/python/pyone/util.py
+++ src/oca/python/pyone/util.py
@@ -177,6 +177,6 @@ class TemplatedType(object):
     def _buildAttributes(self, node, attrs, already_processed):
         vars(self)['custom_attrs'] = dict(attrs)
 
-    def _buildChildren(self, child_, node, nodeName_, fromsubclass_=False, gds_collector_=None):
+    def buildChildren(self, child_, node, nodeName_, fromsubclass_=False, gds_collector_=None):
         if not build_template_node(self, nodeName_, child_):
-            super(TemplatedType, self)._buildChildren(child_,node,nodeName_,fromsubclass_,gds_collector_)
+            super(TemplatedType, self).buildChildren(child_,node,nodeName_,fromsubclass_,gds_collector_)
