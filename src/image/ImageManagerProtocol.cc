9f7a0a7b04356591732bce4921c5c514abcd501c
Christian González <cgonzalez@opennebula.io>
2022-09-08 18:26:29 +0200

    B #5933: honor DRIVER attribute when cloning imgs (#2273)
    Ensure that if the destination DS when cloning an image have a DRIVER set. The image is automatically converted during the cloning process for the fs driver.
    
    (cherry picked from commit 69759b30cc1e7cc429c1bc0cc5df44e070942182)

--- src/image/ImageManagerProtocol.cc
+++ src/image/ImageManagerProtocol.cc
@@ -154,10 +154,18 @@ error_common:
 
 void ImageManager::_clone(unique_ptr<image_msg_t> msg)
 {
-    NebulaLog::dddebug("ImM", "_clone: " + msg->payload());
+    const auto& info = msg->payload();
 
     int     cloning_id;
     int     ds_id = -1;
+    string  source, format;
+
+    ostringstream oss;
+    istringstream is(info);
+    
+    NebulaLog::dddebug("ImM", "_clone: " + info);
+    
+    is >> skipws;
 
     auto image = ipool->get(msg->oid());
 
@@ -165,12 +173,12 @@ void ImageManager::_clone(unique_ptr<image_msg_t> msg)
     {
         if (msg->status() == "SUCCESS")
         {
-            ostringstream oss;
+            is >> source;
 
-            if (!msg->payload().empty())
+            if (!source.empty())
             {
                 oss << "CLONE operation succeeded but image no longer exists."
-                    << " Source image: " << msg->payload()
+                    << " Source image: " << source
                     << ", may be left in datastore";
 
                 NebulaLog::log("ImM", Log::ERROR, oss);
@@ -188,7 +196,21 @@ void ImageManager::_clone(unique_ptr<image_msg_t> msg)
        goto error;
     }
 
-    image->set_source(msg->payload());
+    is >> source;
+
+    if (is.fail())
+    {
+        goto error;
+    }
+
+    image->set_source(source);
+
+    is >> format;
+
+    if (!format.empty())
+    {
+        image->set_format(format);
+    }
 
     image->set_state_unlock();
 
@@ -207,11 +229,7 @@ void ImageManager::_clone(unique_ptr<image_msg_t> msg)
     return;
 
 error:
-    ostringstream oss;
-
     oss << "Error cloning from Image " << cloning_id;
-
-    const auto& info = msg->payload();
 
     if (!info.empty() && (info[0] != '-'))
     {
