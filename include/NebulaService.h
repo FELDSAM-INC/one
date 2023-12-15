f25b08f6c6a804b11cd5b3cc8c06b5574a6b059f
Kristian Feldsam <feldsam@gmail.com>
2023-12-15 16:00:54 +0100

    Bump version to 6.2.2.16
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- include/NebulaService.h
+++ include/NebulaService.h
@@ -150,7 +150,7 @@ public:
      */
     static std::string code_version()
     {
-        return "6.2.2.15"; // bump version
+        return "6.2.2.16"; // bump version
     }
 
     /**
--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -51,7 +51,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '6.2.2.15'
+    VERSION = '6.2.2.16'
 
     # #########################################################################
     # Default location for the authentication file
