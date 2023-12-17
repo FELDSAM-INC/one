4e8eed6507bed5be98ff203cdd36cd3b06b842bc
Kristian Feldsam <feldsam@gmail.com>
2023-12-17 14:23:32 +0100

    Bump version to 6.2.2.22
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -51,7 +51,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '6.2.2.21'
+    VERSION = '6.2.2.22'
 
     # #########################################################################
     # Default location for the authentication file
--- src/onecfg/lib/version.rb
+++ src/onecfg/lib/version.rb
@@ -16,6 +16,6 @@
 
 module OneCfg
 
-    VERSION = '6.2.2.21'
+    VERSION = '6.2.2.22'
 
 end
