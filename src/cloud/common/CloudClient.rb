dc409198789651eb3c87b291ed9995db2a549826
Kristian Feldsam <feldsam@gmail.com>
2024-10-31 10:10:48 +0100

    Bump version to 6.2.2.28
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -51,7 +51,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '6.2.2.27'
+    VERSION = '6.2.2.28'
 
     # #########################################################################
     # Default location for the authentication file
--- src/oca/ruby/opennebula.rb
+++ src/oca/ruby/opennebula.rb
@@ -77,5 +77,5 @@ require 'opennebula/flow'
 module OpenNebula
 
     # OpenNebula version
-    VERSION = '6.2.2.27'
+    VERSION = '6.2.2.28'
 end
