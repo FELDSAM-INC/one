a2299c87d705d083fbffefbb7416f6f78e927de9
Kristian Feldsam <feldsam@gmail.com>
2024-11-01 22:46:15 +0100

    Bump version to 6.2.2.32
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -51,7 +51,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '6.2.2.31'
+    VERSION = '6.2.2.32'
 
     # #########################################################################
     # Default location for the authentication file
--- src/oca/python/setup.py
+++ src/oca/python/setup.py
@@ -40,7 +40,7 @@ install_requires = [
 if sys.version_info[0] < 3:
     install_requires.append('future')
 
-version = '6.2.2.31'
+version = '6.2.2.32'
 
 # mark pre-release
 v1 = int(version.split('.')[1])
