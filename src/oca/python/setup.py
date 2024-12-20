c225477da70c6fb191712c9644a73fab414d473a
Kristian Feldsam <feldsam@gmail.com>
2024-12-20 11:02:55 +0100

    Bump version to 6.2.2.37
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- src/oca/python/setup.py
+++ src/oca/python/setup.py
@@ -40,7 +40,7 @@ install_requires = [
 if sys.version_info[0] < 3:
     install_requires.append('future')
 
-version = '6.2.2.36'
+version = '6.2.2.37'
 
 # mark pre-release
 v1 = int(version.split('.')[1])
--- src/onecfg/lib/version.rb
+++ src/onecfg/lib/version.rb
@@ -16,6 +16,6 @@
 
 module OneCfg
 
-    VERSION = '6.2.2.36'
+    VERSION = '6.2.2.37'
 
 end
